4c336699b4b038688c7907979aa3373e493c0d9a
jongrover <hello@jonathangrover.com>
2014-12-17 14:40:13 -0500

    add solution with updated api endpoint URL

--- js/spotifyChart.js
+++ js/spotifyChart.js
@@ -1,11 +1,43 @@
-function init(){
-  getSpotifyTracks(function(parsed_json) {
-    var tracks = extractTop20Tracks(parsed_json.tracks);
-    var ctx = $("#SpotifyChart").get(0).getContext("2d");
-    var myLineChart = new Chart(ctx).Bar(chartData(extractNames(tracks), extractNumberOfStreams(tracks)), {});
-  });
-}
+jQuery(document).ready(function($) {
 
-$(document).ready(function() {
-  init();
+  var URL = "http://charts.spotify.com/api/tracks/most_streamed/us/weekly/latest";
+  var ctx = $("#SpotifyChart").get(0).getContext("2d");
+  var names = [];
+  var numOfPlays = [];
+
+  $.ajax({
+    url : URL,
+    dataType : "jsonp",
+    success : function(parsed_json) {
+
+      var tracks = parsed_json.tracks.slice(0, 20);
+      var max = tracks.length;
+      for (i = 0; i < max; i++) {
+        names.push(tracks[i].track_name);
+        numOfPlays.push(tracks[i].num_streams)
+      }
+
+      var data = {
+        labels: names,
+        datasets: [
+          {
+            label: "Spotify Chart of Top 20 Streamed Songs on Spotify with their Steam Count",
+            fillColor: "rgba(220,220,220,0.5)",
+            strokeColor: "rgba(220,220,220,0.8)",
+            highlightFill: "rgba(220,220,220,0.75)",
+            highlightStroke: "rgba(220,220,220,1)",
+            data: numOfPlays
+          } 
+        ]
+      };
+
+      var myLineChart = new Chart(ctx).Bar(data, {});    
+
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log(jqXHR);
+      console.log(textStatus);
+      console.log(errorThrown);
+    }
+  });
 });
